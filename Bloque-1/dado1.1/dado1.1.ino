838f73c83ff2c0140964f2c43cce69a5f0213057
Martin Beltran <uo276244@uniovi.es>
2022-09-22 17:13:46 +0200

    Casi la lio

--- Bloque-1/dado1.1/dado1.1.ino
+++ Bloque-1/dado1.1/dado1.1.ino
@@ -2,103 +2,92 @@
   //Martin Beltran Diaz - UO276244
   //Stelian Adrian Stanci - UO277653
   //Laura Pernia Blanco - UO276264
-  // C++ code
-//
-
-//Definimos pines de LEDs
-int pinLed1 = 5;
-int pinLed2 = 6;
-int pinLed3 = 9;
-//Definimos pin del potenciometro
-int pinPoten = 3;
-
-void setup()
-{
-
-Serial.begin(9600);
-Serial.println("Setup");
-  pinMode(pinLed1,OUTPUT);
-  pinMode(pinLed2,OUTPUT);
-  pinMode(pinLed3,OUTPUT);
-  pinMode(pinPoten,INPUT);
   
+  
+  //PINES DE LOS LED
+  int const led1 = 10;
+  int const led2 = 11;
+  int const led3 = 12;
+  int const entradaPulsador = 7;
+   //Solo para que se ejcute una vez cuando se pulsa
+  bool prevEntradaPulsador;
+  int pulsado;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600); // Iniciar el Serial
+  Serial.println("Setup");
+
+  //Definimos los pines como salida
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  pinMode(entradaPulsador, INPUT);
+
+
+  /* Si usamos el pin 0 para generar aleatorios,
+ * este pin deberá de estar vacío, es decir, no puede
+ * tener nada conectado.
+ * Le pasaremos este pin al generador de semillas
+ */
+ int pinRandomSeed = 0;
+ randomSeed(analogRead(pinRandomSeed));
+
+prevEntradaPulsador = false;
 }
 
-int valoresLimiteLeds[][3] = {
-  {1,32,63},
-  {64,100,127},
-  {128,160,191}
-};
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  pulsado = digitalRead(entradaPulsador);
+  
+  if(pulsado == HIGH && !prevEntradaPulsador)
+  {
+    int rand = random(led1,led3 + 1);
+    Serial.print("Pulsado:");
+	Serial.println(pulsado);
+    
+    //Para evitar que una puslacion se registre varias veces 
+    //(porque pulsarlo medio segundo supone varias iteraciones del bucle)
+    prevEntradaPulsador = true;
 
-int valoresComunesLeds[3] = {192,210,255};
-
-int pinesArray[3] = {pinLed1,pinLed2,pinLed3};
-
-void ledProcessPoten(int numeroLed, int inputPoten){
-
-
-  if(inputPoten == 0){
-
-    analogWrite(pinLed1,0);
-    analogWrite(pinLed2,0);
-    analogWrite(pinLed3,0);
-
-  }
-  else if(inputPoten == valoresLimiteLeds[numeroLed-1][0]){
-
-    analogWrite(pinesArray[numeroLed-1], 1);
-
-
-  }else if(inputPoten <= valoresLimiteLeds[numeroLed-1][1]){
-
-    analogWrite(pinesArray[numeroLed-1], 160);
-
-  }else if(inputPoten <= valoresLimiteLeds[numeroLed-1][2]){
-
-    analogWrite(pinesArray[numeroLed-1], 255);
-
-  }else{
-    Serial.print("Value out of bounds: ");
-    Serial.println(inputPoten
-    );
-  }
-
-}
-
-
-
-void loop()
-{
-  int inputPoten = analogRead(pinPoten);
-
-    delay(3000);
-    Serial.print("Input poten: ");
-    Serial.println(inputPoten);
-
-  if(inputPoten < valoresComunesLeds[0]){
-    ledProcessPoten(1, inputPoten);
-    ledProcessPoten(2, inputPoten);
-    ledProcessPoten(3, inputPoten);
-  }
-  else if(inputPoten == valoresComunesLeds[0]){
-
-    for(int i = 0; i < sizeof(pinesArray); i++){
-      analogWrite(pinesArray[i], 1);
+    Serial.print("Pin Aleatorio:");
+    Serial.println(rand);
+    apagarTodas();
+    switch(rand){
+      case led1:
+        digitalWrite(led1, HIGH);
+        break;
+      case led2:
+        digitalWrite(led1, HIGH);
+        digitalWrite(led2, HIGH);
+        break;
+      case led3:
+        digitalWrite(led1, HIGH);
+        digitalWrite(led2, HIGH);
+        digitalWrite(led3,HIGH);
+        break;
     }
     
-
-  }else if(inputPoten <= valoresComunesLeds[2]){
-
-    for(int i = 0; i < sizeof(pinesArray); i++){
-      analogWrite(pinesArray[i], 160);
-    }
-
-  }else if(inputPoten <= valoresComunesLeds[3]){
-
-    for(int i = 0; i < sizeof(pinesArray); i++){
-      analogWrite(pinesArray[i], 255);
-    }
+    Serial.print("Prev Pulsado:");
+    Serial.println(prevEntradaPulsador);
 
   }
+  
+  if(pulsado == LOW){
+     prevEntradaPulsador = false;
+  }
+
+  
+  
+
+}
+
+
+void apagarTodas(){
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3,LOW);
 
 }
