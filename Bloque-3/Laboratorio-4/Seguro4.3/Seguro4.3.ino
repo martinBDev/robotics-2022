f86cdd7a88aa2388872076e742b90c8c2aaf7258
Martin Beltran <UO276244@uniovi.es>
2022-10-08 19:31:18 +0200

    preeliminar Seguro 4.3

--- Bloque-3/Laboratorio-4/Seguro4.3/Seguro4.3.ino
+++ Bloque-3/Laboratorio-4/Seguro4.3/Seguro4.3.ino
@@ -6,6 +6,9 @@ int X_pin = A0; // Pin analógico para leer eje X
 int Y_pin = A1; // Pin analógico para leer eje Y
 
 
+int pin_colision_1 = 2;
+int pin_colision_2 = 3;//Pines de colision
+
 int centro_X = 486;//Valores del centro del joystick que no está exactamente en el centro (512,512)
 int centro_Y = 501;
 int margen = 10; //Margen para detectar movimiento joystick 
@@ -20,37 +23,45 @@ void setup() {
 void loop(){
 
 
-    Serial.println("Boton pulsado:"+String(digitalRead(boton_pin)));
+    bool colisioDelante = digitalRead(pin_colision_2) == 0; //0 si se pulsa
+    bool colisionDetras = digitalRead(pin_colision_1) == 0;
+
+    Serial.println(colisioDelante);
 
     int valorX = analogRead(X_pin);
-    Serial.println("X: "+String(valorX));
 
     int valorY = analogRead(Y_pin);
-    Serial.println("Y: "+String(valorY));
 
 
 
-    if(valorX > centro_X + margen){
-      //4.- Posición giro horario (180) 
-    Serial.println("Enviar: giro en sentido 1");
-    servo.write(180);
+    if(valorX > 913 && !colisioDelante){
+      //rapido delante
+      servo.write(180);
     
-    }else if(valorX < centro_X - margen){
-      Serial.println("Enviar: giro en sentido 2");
-      servo.write(0);
-    }else{
-//6.- Posición detener (90) esperar 2s
-      Serial.println("Enviar: giro en sentido 2");
+    }else if(valorX <= 913 && valorX > centro_X + margen && !colisioDelante){
+
+      // despacio delante
+      servo.write(105);
+    }else if(valorX <= centro_X + margen && valorX >= centro_X - margen ){
+      //Quieto
       servo.write(90);
+    }else if(valorX < centro_X - margen && valorX >= 100 && !colisionDetras){
+      //despacio atras 
+      servo.write(75);
+    }else if( valorX < 100 && !colisionDetras){
+    //rapido atras 
+      servo.write(0);
     }
 
 
     /*
     
+
     //5.- s
     Serial.println("Enviar: detener");
     servo.write(90);
     delay(2000);
+
     // Otros valores entre (0-84) y (93-180) hacen que gire
     //más despacio cuanto más cercano al 90 y más rápido cuanto más
     //cercano al 0 y 180
