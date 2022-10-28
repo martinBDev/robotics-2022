27d4aa587a8387ff5462da52262eb42dd2e90878
Laura Pernía <78539552+laura-pb@users.noreply.github.com>
2022-10-28 16:58:49 +0200

    aaaaa

---
+++ Bloque-4/Laboratorio-7/SigueLineas7.1/SigueLineasPrimero.ino
@@ -0,0 +1,36 @@
+#include <Servo.h>
+int NO_LINE = LOW; // También podría ponerse 0
+int LINE = HIGH; // También podría ponerse 1
+Servo servoLeft;
+Servo servoRight;
+int pinIrDer = 3;
+int pinIrIzq = 2;
+int pinServoRight = 9;
+int pinServoLeft = 8;
+
+void setup(){
+ Serial.begin(9600); // Descomentar si queréis debuguear por consola
+ pinMode(pinIrDer, INPUT);
+ pinMode(pinIrIzq, INPUT);
+
+ servoLeft.attach(pinServoLeft);
+ servoRight.attach(pinServoRight);
+}
+void loop(){
+  //seguir línea
+ if (digitalRead(pinIrIzq) == LINE && digitalRead(pinIrDer) == LINE) {
+  servoLeft.write(0);
+  servoRight.write(40);
+ } else if (digitalRead(pinIrIzq) == LINE && digitalRead(pinIrDer) == NO_LINE) {
+    servoLeft.write(180);
+    servoRight.write(0);
+ } else if (digitalRead(pinIrIzq) == NO_LINE && digitalRead(pinIrDer) == LINE) {
+      servoLeft.write(0);
+    servoRight.write(180);
+ } else if (digitalRead(pinIrIzq) == NO_LINE && digitalRead(pinIrDer) == NO_LINE) {
+      servoLeft.write(90);
+    servoRight.write(90);
+  }
+}
+
+ 
