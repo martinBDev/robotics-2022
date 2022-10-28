ac7da6f993dbd7e5dfd3e1292c0f440f47d09602
Laura Pernía <78539552+laura-pb@users.noreply.github.com>
2022-10-28 16:57:31 +0200

    Add files via upload

--- Bloque-4/Laboratorio-7/SigueLineas7.1/SigueLineas7.1.ino
+++ Bloque-4/Laboratorio-7/SigueLineas7.1/SigueLineas7.1.ino
@@ -1,32 +1,113 @@
 #include <Servo.h>
+int NO_LINE = LOW; // También podría ponerse 0
+int LINE = HIGH; // También podría ponerse 1
 Servo servoLeft;
 Servo servoRight;
-int pinServoLeft = 8;
+int pinIrDer = 3;
+int pinIrIzq = 2;
 int pinServoRight = 9;
+int pinServoLeft = 8;
+
+
+int lineaEncontrada = false;
+int lastLeftMov = 0;
+int lastRightMov = 0;
 
 void setup(){
-  servoLeft.attach(pinServoLeft);
-  servoRight.attach(pinServoRight);
-}
+ Serial.begin(9600); // Descomentar si queréis debuguear por consola
+ pinMode(pinIrDer, INPUT);
+ pinMode(pinIrIzq, INPUT);
 
+ servoLeft.attach(pinServoLeft);
+ servoRight.attach(pinServoRight);
+ 
+}
 void loop(){
-    // Atrás
-    servoLeft.write(180); // Velocidad Máxima
+
+  
+  espiral(lineaEncontrada);
+
+  
+  //corregir trayectoria
+   if(digitalRead(pinIrIzq) == LINE && digitalRead(pinIrDer) == NO_LINE) {
+        servoLeft.write(0);
+        
+        servoRight.write(180);
+        lastLeftMov = 0;
+        lastRightMov = 180;
+   } else if (digitalRead(pinIrIzq) == NO_LINE && digitalRead(pinIrDer) == LINE) {
+        servoLeft.write(180);
+        servoRight.write(0);
+        lastLeftMov = 180;
+        lastRightMov = 0;
+   }
+
+  //seguir línea
+ if (digitalRead(pinIrIzq) == LINE && digitalRead(pinIrDer) == LINE) {
+  servoLeft.write(0);
+  servoRight.write(40);
+  lastLeftMov = 0;
+        lastRightMov = 40;
+ } else if (digitalRead(pinIrIzq) == LINE && digitalRead(pinIrDer) == NO_LINE) {
+    servoLeft.write(180);
     servoRight.write(0);
-    delay(3000);
-    servoLeft.write(100); // Próximo a 90, despacio
-    servoRight.write(80);
-    delay(3000);
-
-    servoLeft.write(90); // Detener
-    servoRight.write(90);
-    delay(3000);
-
-    // Adelante
-    servoLeft.write(80); // Próximo a 90, despacio
-    servoRight.write(100);
-    delay(3000);
-    servoLeft.write(0); // Velocidad Máxima
+    lastLeftMov = 180;
+        lastRightMov = 0;
+ } else if (digitalRead(pinIrIzq) == NO_LINE && digitalRead(pinIrDer) == LINE) {
+      servoLeft.write(0);
     servoRight.write(180);
-    delay(3000);
+    lastLeftMov = 0;
+        lastRightMov = 180;
+ } else if (digitalRead(pinIrIzq) == NO_LINE && digitalRead(pinIrDer) == NO_LINE) {
+      servoLeft.write(180 - lastLeftMov);
+      servoRight.write(180 - lastRightMov);
+    
+  }
 }
+
+void espiral(bool hayLinea){
+  if(!hayLinea){
+
+    
+  int rango = 0;
+  long millis1;
+  while (digitalRead(pinIrIzq) == NO_LINE && digitalRead(pinIrDer) == NO_LINE) {
+
+      
+        //Palante
+        servoLeft.write(0);
+        servoRight.write(0);
+        millis1 = millis();
+
+        //Palante 3 segundos
+        while(millis() - millis1 < 3000){
+          if(digitalRead(pinIrIzq) == LINE || digitalRead(pinIrDer) == LINE){
+            lineaEncontrada = true;
+            return;
+          }
+        }
+        
+        //pATRÁ
+        servoLeft.write(180);
+        servoRight.write(180);
+        //Patras 3 segundos
+        while(millis() - millis1 < 3000){
+          if(digitalRead(pinIrIzq) == LINE || digitalRead(pinIrDer) == LINE){
+            lineaEncontrada = true;
+            return;
+          }
+        }
+
+       
+        //Giro pa un lao
+        servoLeft.write(0);
+        servoRight.write(180);
+        delay(500);
+
+
+        
+    }
+    
+  }
+}
+ 
